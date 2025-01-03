a734d8321b6e740f01ef8f18b3a4c8a9b63efdea
Banana Duck <alhanaqtah.dev@gmail.com>
2025-01-04 01:28:34 +0300

    tokens extended

--- internal/token/token.go
+++ internal/token/token.go
@@ -7,8 +7,18 @@ const (
 	IDENT = "ID"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	// OPERATORS
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	BANG     = "!"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	LT = "<"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -18,13 +28,44 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	// KEYWORDS
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 type TokenType string
 
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
 type Token struct {
-	Token   TokenType
+	Type    TokenType
 	Literal string
+}
+
+func New(tokenType TokenType, literal string) Token {
+	return Token{
+		Type:    tokenType,
+		Literal: literal,
+	}
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
 }
